9d8ebff1747f96d3749b284b3102fb55656be1c8
Yaroslav <yaroslavkopylov98@gmail.com>
2022-10-21 18:28:28 +0200

    finaly promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
+import Notiflix from "notiflix";
+const firstDelay = document.querySelector('input[name="delay"]'),
+    delayStep = document.querySelector('input[name="step"]'),
+    amount = document.querySelector('input[name="amount"]'),
+    btnStart = document.querySelector("button[type='submit']");
+// notiflixAlert = document.getElementById("NotiflixNotifyWrap-1");
+// console.log(notiflixAlert);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    return new Promise((resolve, reject) => {
+        const shouldResolve = Math.random() > 0.3;
+
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({ position, delay });
+            } else {
+                reject({ position, delay });
+            }
+        }, delay);
+    });
 }
+
+btnStart.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let delay = Number(firstDelay.value),
+        step = Number(delayStep.value);
+
+    for (let i = 0; i < amount.value; i++)
+        createPromise(1 + i, delay + i * step)
+            .then(({ position, delay }) => {
+                Notiflix.Notify.success(
+                    `✅ Fulfilled promise ${position} in ${delay}ms`
+                );
+            })
+            .catch(({ position, delay }) => {
+                Notiflix.Notify.failure(
+                    `❌ Rejected promise ${position} in ${delay}ms`
+                );
+            });
+});
+
+// notiflixAlert.addEventListener("mousemove", onRemove);
+
+// function onRemove() {
+//     removeVisible();
+// }
+
+// function removeVisible() {
+//     notiflixAlert.classList.add("nx-remove");
+// }
